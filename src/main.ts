66fe417ef548cdde2396a06a2b1035b8b89cd21b
ahmed <ahmed.abdelkader.khedr@gmail.com>
2025-05-02 03:55:27 +0300

    ver0.8

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: (origin, callback) => {
-      const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:3000'];
+      const allowedOrigins = ['http://localhost:5173/', 'http://localhost:3001/', 'http://localhost:3000/'];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
