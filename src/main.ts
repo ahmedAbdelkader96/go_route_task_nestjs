dc2ff9858a3bebafa7a7212ea977306faa5c7b67
ahmed <ahmed.abdelkader.khedr@gmail.com>
2025-05-02 05:12:46 +0300

    ver0.23

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:5173', // Allow requests from your React app
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // Allow cookies if needed
   });
   app.useGlobalFilters(new HttpExceptionFilter());
