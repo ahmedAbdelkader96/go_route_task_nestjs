f520de53f15b24faa18bdff25729585541c3bf9b
ahmed <ahmed.abdelkader.khedr@gmail.com>
2025-05-02 03:28:27 +0300

    ver0.0

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3001', // Replace with your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new LoggingInterceptor()); // Apply globally
   app.useGlobalPipes(new ValidationPipe());
